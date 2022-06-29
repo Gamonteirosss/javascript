0aebe198ab93e81c590ccd72d60926fd8e45dcc2
Gabriel Henrique de Paula Monteiro <gamonteiros@outlook.com>
2022-06-29 16:24:26 -0300

    Update script.js

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -18,22 +18,29 @@ if (fano.value.length == 0 || fano.value > ano) {
             img.setAttribute('src', 'menino.png')
         } else if (idade < 21) {
             // Jovem
+            img.setAttribute('src', 'garoto.png')
         } else if (idade < 50 ){
             // Adulto
+            img.setAttribute('src', 'homem.png')
         } else {
             // Idoso
+            img.setAttribute('src', 'idosa.png')
         }
     
     } else if (fsex[1].checked) {
         gênero ='Mulher'
         if (idade >= 0 && idade <10) {
             // Criança
+            img.setAttribute('src', 'menina.png')
         } else if (idade < 21) {
             // Jovem
+            img.setAttribute('src', 'garota.png')
         } else if (idade < 50 ){
             // Adulto
+            img.setAttribute('src', 'mulher.png')
         } else {
             // Idoso
+            img.setAttribute('src', 'idosa.png')
         }
         
     }
